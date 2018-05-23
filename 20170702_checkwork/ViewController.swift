ce77286faa90e9fa6e9bc8b6271af2d22db577ac
Ririkapi <lilyflower0425@gmail.com>
2018-05-23 15:41:12 +0900

    Initial Commit

---
+++ 20170702_checkwork/ViewController.swift
@@ -0,0 +1,37 @@
+    //
+    //  ViewController.swift
+    //  20170702_checkwork
+    //
+    //  Created by 中澤郁斗 on 2017/06/26.
+    //  Copyright © 2017年 中澤郁斗. All rights reserved.
+    //
+    
+    import UIKit
+    
+    class ViewController: UIViewController {
+        
+        var number = 0
+        @IBOutlet var countLabel: UILabel!
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        @IBAction func plus() {
+            number += 1
+            countLabel.text = String(number)
+        }
+        
+        @IBAction func minus() {
+            number -= 1
+            countLabel.text = String(number)
+        }
+        
+    }
+    
+
